1d21b180030b5c27382fda4374b9276c49067fa0
vidhyapb <pbvidhyauk@gmail.com>
2020-12-29 17:14:22 +0530

    This is the second commit into the repo which includes the main source code.

---
+++ main.py
@@ -0,0 +1,6 @@
+for x in range(1, 10, 1):   # Start, Stop, Incremental Range
+    print('*' * x)
+for y in range(1, 25, 1):   # Start, Stop, Incremental Range
+    print('|' * y)
+for z in range(1, 15, 1):   # Start, Stop, Incremental Range
+    print('.' * z)
